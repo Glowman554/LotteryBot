4a9dbffeba51c315b47a6b0e0a9dde58344ba0e9
janick voss <vossjanick@gmail.com>
2021-01-21 10:06:17 +0100

    Add command line interface

--- LotteryBot/LotteryBot.cpp
+++ LotteryBot/LotteryBot.cpp
@@ -1,7 +1,21 @@
 ﻿#include <core.h>
+#include <tclap/CmdLine.h>
+
+int maxTickets;
+int klickPerSeconds;
+
+int main(int argc, char** argv) {
+    TCLAP::CmdLine cmd("LotteryBot", ' ', "0.1", true);
+    TCLAP::ValueArg<int> maxTicketsArg("m", "maxTickets", "maxTickets", false, 10, "int");
+    TCLAP::ValueArg<int> klickPerSecondsArg("k", "klickPerSeconds", "klickPerSeconds", false, 20, "int");
+    cmd.add(maxTicketsArg);
+    cmd.add(klickPerSecondsArg);
+    cmd.parse(argc, argv);
 
 
-int main() {
+    maxTickets = maxTicketsArg.getValue();
+    klickPerSeconds = klickPerSecondsArg.getValue();
+
     HWND hWnd = FindWindowW(nullptr, MINECRAFT_VER);
     if (hWnd) {
         SetWindowPos(hWnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
@@ -10,7 +24,7 @@ int main() {
         MoveWindow(hWnd, 0, 0, WINDOW_X, WINDOW_Y, true);
 
         Sleep(100);
-        std::thread bot_thread([] { return StartBot(10, 20); });
+        std::thread bot_thread([] { return StartBot(maxTickets, klickPerSeconds); });
         std::thread shutdown_thread([] { return ShutdownHook(); });
 
         bot_thread.join();
